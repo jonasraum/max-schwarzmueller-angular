23196ecadfd1e0d15c23d5e342bb633b98d6f414
Jonas Raum <75501665+jonasraum@users.noreply.github.com>
2021-03-09 09:57:08 +0100

    67 - binding to custom properties

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
-  
+  serverElements = [];
   newServerName = '';
   newServerContent = '';
 
